7f8634152843a1604923ef12566b620f344f4964
Tanat Tanalai <kootiktok@Tanats-MacBook-Pro.local>
2020-12-31 22:12:46 +0700

    change travis public

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Hi <code>src/App.js</code> is here.
+          Hello <code>src/App.js</code> is here.
         </p>
         <a
           className='App-link'
